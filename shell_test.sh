10d10fcf6c5ff8a0b16497eb0d674c86510774d7
Jens Brauer <jensb89@users.noreply.github.com>
2020-08-15 14:03:04 +0200

    Deleted unused files + added Travis CI job

--- shell_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Running..."
-output=$(python "download_cutlist.py" "asd" 2>&1) #http://stackoverflow.com/questions/11900828/store-return-value-of-a-python-script-in-a-bash-script
-echo "Finished"
-echo "$output"
-echo "Output <"
-echo $? #error code from python script http://stackoverflow.com/questions/285289/exit-codes-in-python
